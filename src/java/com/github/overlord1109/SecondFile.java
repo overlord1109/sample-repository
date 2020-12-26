76a2f45790b5120714b1faa07ffce0bdfc12fe4a
Mandar Mahajan <mandarmahajan1996@gmail.com>
2020-12-26 20:05:27 +0530

    Method parameter removed

--- src/java/com/github/overlord1109/SecondFile.java
+++ src/java/com/github/overlord1109/SecondFile.java
@@ -2,12 +2,12 @@ package com.github.overlord1109;
 
 public class SecondFile {
 
-    //New method added
-    public static int add(int x, int y, int z) {
+    //Method parameter removed
+    public static int add(int x, int z) {
         return x + y + z;
     }
 
     public static void main(String[] args) {
-        System.out.println("Addition is: " +  add(1 + 2 + 3));
+        System.out.println("Addition is: " +  add(1 + 3));
     }
 }
